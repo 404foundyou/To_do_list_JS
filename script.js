2ce171584e53924752cb2a6db6f93c4b0d9db1f8
404foundyou <attendrabhushan@gmail.com>
2026-01-18 00:25:17 +0530

    delete btn added

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-let todoinput = document.getElementById("to_do_input");
-let addtaskbtn = document.getElementById("add_task_btn");
-let listitem = document.getElementById("list_item");
+document.addEventListener('DOMContentLoaded',()=>{
+    let todoinput = document.getElementById("to_do_input");
+    let addtaskbtn = document.getElementById("add_task_btn");
+    let todolist = document.getElementById("list_item");
+    
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-let tasks = [];
+    tasks.forEach(task => rendertask(task));
+    
+    addtaskbtn.addEventListener('click',()=>{
+        const task_text = todoinput.value.trim();
+        if(task_text === "") return;  
+        
+        const new_task = {
+            id: Date.now(),
+            text: task_text,
+            completed: false
+        }
+        
+        tasks.push(new_task);
+        saveTask();
+        rendertask(new_task);
+        todoinput.value = "" ; // clear input
+        console.log(tasks);
+        
+    });
+    
 
-addtaskbtn.addEventListener('click',()=>{
-    const task_text = todoinput.value.trim();
-    if(task_text === "") return;  
+    function rendertask(task){
+        const li = document.createElement("li");
+        li.setAttribute('data-id',task.id);
+        li.innerHTML = `
+        <span>${task.text}</span>
+        <button>Delete</button>
+        `;
+        todolist.appendChild(li);
 
-    const new_task = {
-        id: Date.now(),
-        text: task_text,
-        completed: false
     }
-    tasks.push(new_task);
-    todoinput.value = "" ; // clear input
-    console.log(tasks);
-});
+    
+    function saveTask(){
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+})
+
+
